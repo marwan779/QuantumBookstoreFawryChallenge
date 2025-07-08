62dcc35b55e78f1800defacf3cf381f27f9be682
Marwan <UGS.16220338@eng.suez.edu.eg>
2025-07-08 17:42:37 +0300

    Creating PaperBook, Extending BaseBook

---
+++ Models/PaperBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantumBookstoreFawryChallenge.Models
+{
+    public class PaperBook : BaseBook
+    {
+        public int Stock {  get; set; }
+        public PaperBook(string iSBN, string title, int publishingYear, double price) 
+            : base(iSBN, title, publishingYear, price)
+        {
+        }
+    }
+}
