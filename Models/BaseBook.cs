89bb6125f895dd16816578e1ba80ace97cdb1865
Marwan <UGS.16220338@eng.suez.edu.eg>
2025-07-08 17:38:43 +0300

    Creating BaseBook Class

---
+++ Models/BaseBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantumBookstoreFawryChallenge.Models
+{
+    public class BaseBook
+    {
+        public string ISBN { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public int PublishingYear { get; set; }
+        public double Price { get; set; }
+
+        public BaseBook(string iSBN, string title, int publishingYear, double price)
+        {
+            ISBN = iSBN;
+            Title = title;
+            PublishingYear = publishingYear;
+            Price = price;
+        }
+    }
+}
